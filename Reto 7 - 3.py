a446883fb2a73619b0e12729eb0668dbc149daa4
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-22 14:29:09 -0500

    Add files via upload

---
+++ Reto 7 - 3.py
@@ -0,0 +1,7 @@
+n: int = 50
+
+while n >= 2:
+    if n % 2 == 0:
+        print(n)
+    n -= 2
+    print(str(n))
---
+++ Reto 7 - 8.py
@@ -0,0 +1,25 @@
+# Definir la funcion para calcular los numeros primos de 1 hasta a 100
+def calcularprimos(numero):
+    
+# Si el numero es divisible entre 2 y su raiz cuadrada, no es primo
+    i = 2
+    while i*i <= numero:
+      if numero % i == 0:
+        return False
+      
+# Por el contrario, si no es divisible, es primo
+      i += 1
+    return True
+
+# Lamar a la funcion para verificar si el numero es primo
+def numerosprimos():
+    print("Números primos del 1 al 100: ")
+    num = 2
+
+# Definir la condicion del bucle
+    while num <= 100:
+        if calcularprimos(num):
+            print(num)
+        num += 1
+
+numerosprimos()
